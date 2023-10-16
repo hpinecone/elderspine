cc1e207b4460cb5fe1a6bfdf0892276b81f000e2
hpinecone <thehackingpinecone@gmail.com>
2023-10-16 17:05:10 -0400

    13

--- src/main/Main.java
+++ src/main/Main.java
@@ -3,21 +3,27 @@ package main;
 import  javax.swing.JFrame;
 public class Main {
     public static void main(String[] args) {
-        JFrame window = new JFrame(); //jframe start
-        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //makes the game closable
-        window.setResizable(true); //resizable
-
-        GamePanel gamePanel = new GamePanel(); //instantates the game panel class
-        window.add(gamePanel);
-        window.pack();
-        window.setLocationRelativeTo(null);
-        window.setVisible(true); //window visible
-        window.setExtendedState(JFrame.MAXIMIZED_BOTH); //starts maximized/manipulate with bool
-        //jframe end
-
-        gamePanel.startGameThread(); //calls start operation
+        System.out.println("starting :3");
+    display();
 
     }
+public static void display(){
+    JFrame window = new JFrame(); //jframe start
+    window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //makes the game closable
+    window.setResizable(true); //resizable
+
+    GamePanel gamePanel = new GamePanel(); //instantates the game panel class
+    window.add(gamePanel);
+    window.pack();
+    window.setLocationRelativeTo(null);
+    window.setVisible(true); //window visible
+    window.setExtendedState(JFrame.MAXIMIZED_BOTH); //starts maximized/manipulate with bool
+    window.setTitle("Elder Spine - Alpha");
+    //jframe end
+
+    gamePanel.startGameThread(); //calls start operation
+
+}
 }
 /*
     inspired/borrowed from RyiSnow https://www.youtube.com/@RyiSnow <3
